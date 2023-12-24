93a867311106144924151b86edf4b44c344354a5
dipannita5 <dipannita.roy@travclan.com>
2023-12-24 14:41:12 +0530

    Changes cursor pointer events.

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,76 @@ import {
   Works,
   StarsCanvas,
 } from "./components";
+import { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    const coords = { x: 0, y: 0 };
+    const circles = document.querySelectorAll(".circle");
+
+    const colors = [
+      "#ffb56b",
+      "#fdaf69",
+      "#f89d63",
+      "#f59761",
+      "#ef865e",
+      "#ec805d",
+      "#e36e5c",
+      "#df685c",
+      "#d5585c",
+      "#d1525c",
+      "#c5415d",
+      "#c03b5d",
+      "#b22c5e",
+      "#ac265e",
+      "#9c155f",
+      "#950f5f",
+      "#830060",
+      "#7c0060",
+      "#680060",
+      "#60005f",
+      "#48005f",
+      "#3d005e",
+    ];
+
+    circles.forEach(function (circle, index) {
+      circle.x = 0;
+      circle.y = 0;
+      circle.style.backgroundColor = colors[index % colors.length];
+    });
+
+    window.addEventListener("mousemove", function (e) {
+      coords.x = e.clientX;
+      coords.y = e.clientY;
+    });
+
+    function animateCircles() {
+      let x = coords.x;
+      let y = coords.y;
+
+      circles.forEach(function (circle, index) {
+        circle.style.left = x - 12 + "px";
+        circle.style.top = y - 12 + "px";
+
+        circle.style.scale = (circles.length - index) / circles.length;
+
+        circle.x = x;
+        circle.y = y;
+
+        const nextCircle = circles[index + 1] || circles[0];
+        x += (nextCircle.x - x) * 0.3;
+        y += (nextCircle.y - y) * 0.3;
+      });
+
+      requestAnimationFrame(animateCircles);
+    }
+
+    animateCircles();
+  }, []);
 
   return (
     <BrowserRouter>
-      <AnimatedCursor
+      {/* <AnimatedCursor
         innerSize={20}
         // onPointerEnter={handlePointerEnter}
         // onPointerLeave={handlePointerLeave}
@@ -29,11 +93,6 @@ const App = () => {
         // innerStyle={{ backgroundColor: "transparent" }}
         clickables={[
           "a",
-          'input[type="text"]',
-          'input[type="email"]',
-          'input[type="number"]',
-          'input[type="submit"]',
-          'input[type="image"]',
           "label[for]",
           "select",
           "textarea",
@@ -52,8 +111,9 @@ const App = () => {
             },
           },
         ]}
-      />
-      <div className="relative z-0 bg-primary">
+      /> */}
+      {Array(22).fill(0).map((item, index) => {return(<div key={index} class="circle"></div>)})}
+      <div id="dom" className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,27 @@ import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
+// require('dotenv').config()
+
+const SuccessSnackBar = () => {
+  return(<div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+  <div class="relative p-4 w-full max-w-2xl max-h-full">
+      <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+          <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+              <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                  Terms of Service
+              </h3>
+              <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                  <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                  </svg>
+                  <span class="sr-only">Close modal</span>
+              </button>
+          </div>
+        </div>
+  </div>
+</div>)
+}
 
 const Contact = () => {
   const formRef = useRef();
@@ -14,6 +35,7 @@ const Contact = () => {
     email: "",
     message: "",
   });
+  const [isMailSent, setIsMailSent] = useState(false);
 
   const [loading, setLoading] = useState(false);
 
@@ -26,7 +48,7 @@ const Contact = () => {
       [name]: value,
     });
   };
-
+  console.log("inside form str222", import.meta.env)
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
@@ -47,8 +69,8 @@ const Contact = () => {
       .then(
         () => {
           setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
-
+          // alert("Thank you. I will get back to you as soon as possible.");
+          setIsMailSent(true);
           setForm({
             name: "",
             email: "",
@@ -118,11 +140,11 @@ const Contact = () => {
             type='submit'
             className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
           >
-            <a href={`mailto:${form.email}`}>{loading ? "Sending..." : "Send"}</a>
+            {loading ? "Sending..." : "Send"}
           </button>
         </form>
       </motion.div>
-
+      {isMailSent && <SuccessSnackBar/>}
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
         className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
